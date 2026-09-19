67a9cacd439eb533e860a31f2c198e6dc6fa69d3
Suraj <mailme.surajsen@gmail.com>
2026-09-19 20:49:26 +0530

    update9j

--- app/layout.js
+++ app/layout.js
@@ -40,7 +40,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5779676733459906" crossOrigin="anonymous"></script>
+        <script async custom-element="amp-auto-ads" src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js"></script>
+      </head>
       <body className={`${jakarta.variable} ${jakarta.className} bg-background text-foreground antialiased`}>
+        <amp-auto-ads type="adsense" data-ad-client="ca-pub-5779676733459906"></amp-auto-ads>
         <AuthProvider>
           {children}
         </AuthProvider>
